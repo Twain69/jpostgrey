9ab8e429493554be9338aa71039584eb4825674a
Oliver Flegler <oliver@flegler.com>
2015-04-27 23:58:45 +0200

    Fixed RPM upload source path

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-spacewalk_uploader.sh -s http://spacewalk.flegler.com/ -u $SPACEWALK_USER -P $SPACEWALK_PASSWORD -c flegler_64bit -l flegler_32bit -z $WORKSPACE/target/rpm/pymon/RPMS/noarch/*rpm
+spacewalk_uploader.sh -s http://spacewalk.flegler.com/ -u $SPACEWALK_USER -P $SPACEWALK_PASSWORD -c flegler_64bit -l flegler_32bit -z $WORKSPACE/target/rpm/jpostgrey/RPMS/noarch/*rpm
