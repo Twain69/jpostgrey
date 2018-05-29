d1486d7c8e91e17c2f5cc04f387a066f93b9d3fe
Oliver Flegler <oliver@flegler.com>
2018-05-29 21:12:30 +0200

    Removed run mode and made the tool daemon only Changed from SysInitV to systemd

--- upload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-spacewalk_uploader.sh -s http://spacewalk.flegler.com/ -u $SPACEWALK_USER -P $SPACEWALK_PASSWORD -c flegler_64bit -l flegler_32bit -z $WORKSPACE/target/rpm/jpostgrey/RPMS/noarch/*rpm
