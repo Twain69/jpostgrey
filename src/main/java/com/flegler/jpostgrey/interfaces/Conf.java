eb96990166724e05dc9cce2fae4b0623592e9e12
Oliver Flegler <oliver@flegler.com>
2018-05-29 22:05:02 +0200

    Changed default data fetcher to memory fetcher

--- src/main/java/com/flegler/jpostgrey/interfaces/Conf.java
+++ src/main/java/com/flegler/jpostgrey/interfaces/Conf.java
@@ -18,7 +18,7 @@ public interface Conf extends Mutable {
     String bindAddress();
 
     @Key("application.datafetcher.type")
-    @DefaultValue("com.flegler.jpostgrey.dataFetcher.DatabaseDataFetcher")
+    @DefaultValue("com.flegler.jpostgrey.dataFetcher.MemoryDataFetcher")
     String dataClassName();
 
     @Key("application.greylisting.time")
